44612944fe5dbd83deab56a03837381f6688d4d6
Daniel Antunes <daniel.antunes.nutri@gmail.com>
2023-05-13 13:56:19 -0300

    Foram feitas alterações no código para melhor funcionamento, manutenção, e também uso de cores de forma mais inteligente.

--- JogoDaForca.py
+++ JogoDaForca.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 
 def desenho(qtd_erros):
@@ -7,48 +7,52 @@ def desenho(qtd_erros):
               '\\O/ \n |\n/',
               '\\O/ \n |\n/ \\',
               ' _O_ \n/ | \ \n / \\']
-
     while True:
         print()
-        print('*'*20)
-        print(cores['amarelo_negrito'], end='')
+        print('*' * 20)
+        colorir('amarelo_negrito')
         print(f'{boneco[qtd_erros]}')
-        print(cores['limpa'], end='')
-        print('*'*20)
+        colorir('limpa')
+        print('*' * 20)
         print()
         qtd_erros += 1
         break
 
 
 def mensagem_com_linhas(msg):
-    print('-'*40)
+    print('-' * 40)
     print(msg)
-    print('-'*40)
+    print('-' * 40)
 
 
-cores = {'limpa': '\033[m',
+def colorir(cor):
+    cores = {'limpa': '\033[m',
              'verde_negrito': '\033[1;32m',
              'vermelho_negrito': '\033[1;31m',
              'branco_negrito': '\033[1;97m',
              'azul_negrito': '\033[1;34m',
              'amarelo_negrito': '\033[1;33m'}
+    print(cores[cor])
 
 
-palavras = ['banana', 'uva', 'abacaxi', 'morango', 'abacate', 'amora',
-            'framboesa', 'pêra', 'maçã', 'laranja', 'jaca', 'goiaba', 'acerola',
-            'limão', 'tangerina', 'maracujá', 'ameixa', 'mamão', 'coco', 'figo']
-escolhida = palavras[randint(0, len(palavras) - 1)]
+palavras = {'frutas': ['banana', 'uva', 'abacaxi', 'morango', 'abacate', 'amora',
+                       'framboesa', 'pêra', 'maçã', 'laranja', 'jaca', 'goiaba', 'acerola',
+                       'limão', 'tangerina', 'maracujá', 'ameixa', 'mamão', 'coco', 'figo']}
+
+#No dicionário palavras, estou pegando os values de 'frutas', e randomizando um valor dessa lista (value).
+#escolhida = palavras['frutas'][randint(0, len(palavras['frutas']) - 1)]
+escolhida = choice(palavras['frutas'])
 print()
 print(f'A fruta escolhida tem {len(escolhida)} letras: ', end='')
-print('_'*len(escolhida))
+print('_' * len(escolhida))
 
-#Fazendo a lista com underlines, de acordo com a palavra escolhida.
+# Fazendo a lista com underlines, de acordo com a palavra escolhida.
 escondida = []
 for c in range(0, len(escolhida)):
     escondida.append('_')
-#print(escondida)
+# print(escondida)
 
-#Variável para controle de quantas vezes o usuário vai errar.
+# Variável para controle de quantas vezes o usuário vai errar.
 erros = 0
 while True:
     print()
@@ -60,15 +64,19 @@ while True:
         desenho(erros)
         erros += 1
     for letra in escondida:
-        print(letra, end='')
+        if letra != '_':
+            print(f'\033[0;34m{letra}\33[m', end='')
+        else:
+            print(letra, end='')
     print()
     if erros < 7:
         if '_' not in escondida:
             mensagem_com_linhas('Jogo finalizado!')
+            colorir('verde_negrito')
             print('\nVocê venceu! ')
             break
     else:
         mensagem_com_linhas('Jogo finalizado!')
+        colorir('vermelho_negrito')
         print(f'\nVocê perdeu! A palavra era {escolhida}')
         break
-
